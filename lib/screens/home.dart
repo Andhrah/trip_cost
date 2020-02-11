21ae80e3fc2fda55148d819b01d9d130023bfdfb
Alexandra Collins <andracolexa@gmail.com>
2020-02-11 01:50:05 +0100

    modularize app
    - implement home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HelloYou extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HelloYouState();
+}
+
+class _HelloYouState extends State<HelloYou> {
+  String name = '';
+  final _currencies = ['Dollars', 'Euro', 'Pounds', 'Naira'];
+  String _currency = 'Dollars';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text("Hello"),
+      backgroundColor: Colors.blueAccent,
+    ),
+    body: Container(
+      padding: EdgeInsets.all(15.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Please insert your name',
+            ),
+            onChanged: (String string) {
+              setState(() {
+                name = string;
+              });
+            },
+          ),
+          DropdownButton<String>(
+            items: _currencies.map((String value){
+              return DropdownMenuItem<String> (
+                value: value,
+                child: Text(value)
+              );
+            }).toList(),
+            value: _currency,
+            onChanged: (String value) {
+              _onDropdownChanged(value);
+            },
+            ),
+          Text('Hello ' + name + '!')
+        ],
+      )
+    )
+  );
+  }
+  _onDropdownChanged(String value) {
+    setState(() {
+      this._currency = value;
+    });
+  }
+}
